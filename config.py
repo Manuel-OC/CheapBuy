e41b1edccd876fa4c8c78e93e09f2586c35df3b2
Manuel-OC <41697773+Manuel-OC@users.noreply.github.com>
2025-06-08 18:21:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SUPABASE_URL = "https://<tu-proyecto>.supabase.co"
+SUPABASE_URL = "https://rmkfdfyhfjnxmxqwfcai.supabase.co"
 SUPABASE_KEY = "<anon-key-o-service-role>"
 SUPER_MAP = {
     "Mercadona": "mercadona",
