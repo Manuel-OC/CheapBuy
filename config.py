aa945f272fd42bfe2774fc32da8530c23e3e2112
Manuel-OC <41697773+Manuel-OC@users.noreply.github.com>
2025-06-08 17:51:01 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+SUPABASE_URL = "https://<tu-proyecto>.supabase.co"
+SUPABASE_KEY = "<anon-key-o-service-role>"
+SUPER_MAP = {
+    "Mercadona": "mercadona",
+    "Dia": "dia",
+    "Carrefour": "carrefour"
+}
