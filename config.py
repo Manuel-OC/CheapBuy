62228002d57ae5048e4e56e867ce806696334cbe
Manuel-OC <41697773+Manuel-OC@users.noreply.github.com>
2025-06-08 18:25:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 SUPABASE_URL = "https://rmkfdfyhfjnxmxqwfcai.supabase.co"
-SUPABASE_KEY = "<anon-key-o-service-role>"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJta2ZkZnloZmpueG14cXdmY2FpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0OTMyMjE3OSwiZXhwIjoyMDY0ODk4MTc5fQ.-O5oDD8IIbMNLFlOpts4qcU_RBKMkR7AxA-stESWLy0"
 SUPER_MAP = {
     "Mercadona": "mercadona",
     "Dia": "dia",
