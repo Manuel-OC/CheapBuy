fa38c578f58ea9cc22351b34acf054c34b6f9b2e
Manuel-OC <41697773+Manuel-OC@users.noreply.github.com>
2025-06-14 13:55:57 +0200

    Update importarcsv.py

--- importarcsv.py
+++ importarcsv.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from supabase import create_client, Client
+import os
 
 #Conecto con Supabase
-SUPABASE_URL = "https://rmkfdfyhfjnxmxqwfcai.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJta2ZkZnloZmpueG14cXdmY2FpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0OTMyMjE3OSwiZXhwIjoyMDY0ODk4MTc5fQ.-O5oDD8IIbMNLFlOpts4qcU_RBKMkR7AxA-stESWLy0"
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 TABLE_NAME = "producto"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
